acef22ca1942d975b42e2c8aaf52e7e5435d8428
Ghostfighter50 <ghostfighter5015@gmail.com>
2022-05-14 13:03:41 +0200

    Added Protected Routes

--- server/server.ts
+++ server/server.ts
@@ -3,13 +3,17 @@ import session from 'express-session'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
+import fs from 'fs'
 import DBconnect from './src/Config/Database'
 import router from './src/Routes/Auth'
 import { Logger } from './src/Utilities/Logger'
 DBconnect()
 const app = express()
 app.use(bodyParser.json())
-app.use(morgan('dev'))
+app.use(morgan('common', {
+  stream: fs.createWriteStream('./logs/access.log', {flags: 'a+'}),
+  
+}))
 app.use(cors({
   origin: [process.env.ORIGIN || 'http://localhost:3000'],
   methods: ['GET', 'POST', 'DELETE', 'PUT'],
